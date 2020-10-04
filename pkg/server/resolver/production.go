cf8fea537b4e6df203333ce5169ebf74b5527206
SaKu2110 <b1018106@fun.ac.jp>
2020-10-04 23:59:25 +0900

    [Fix] regenerate production.go

--- pkg/server/resolver/production.go
+++ pkg/server/resolver/production.go
@@ -9,11 +9,17 @@ import (
 	"github.com/miraikeitai2020/backend-bff/pkg/server/model"
 )
 
-func (r *mutationResolver) Signup(ctx context.Context, id string, password string) (string, error) {
+type Resolver struct{}
+
+func (r *mutationResolver) Signup(ctx context.Context, id string, password string) (*model.Token, error) {
 	panic("not implemented")
 }
 
-func (r *mutationResolver) AddUserInfo(ctx context.Context, token string, gender int, name string, year int, month int) (*model.Result, error) {
+func (r *mutationResolver) AddConstantUserInfo(ctx context.Context, token string, gender int, year int, month int) (*model.Result, error) {
+	panic("not implemented")
+}
+
+func (r *mutationResolver) AddName(ctx context.Context, token string, name string) (*model.Result, error) {
 	panic("not implemented")
 }
 
@@ -41,7 +47,7 @@ func (r *mutationResolver) AddNewLogData(ctx context.Context, id string, date st
 	panic("not implemented")
 }
 
-func (r *mutationResolver) AddSubscription(ctx context.Context, id string) (*model.Subsc, error) {
+func (r *mutationResolver) AddSubscription(ctx context.Context, id string) (*model.Result, error) {
 	panic("not implemented")
 }
 
@@ -53,15 +59,27 @@ func (r *mutationResolver) AddSpot(ctx context.Context, name string, description
 	panic("not implemented")
 }
 
-func (r *queryResolver) Signin(ctx context.Context, id string, password string) (string, error) {
+func (r *queryResolver) Signin(ctx context.Context, id string, password string) (*model.Token, error) {
 	panic("not implemented")
 }
 
-func (r *queryResolver) UserInfo(ctx context.Context, token string) (*model.UserHome, error) {
+func (r *queryResolver) UserInfo(ctx context.Context, token string) (*model.UserInfo, error) {
 	panic("not implemented")
 }
 
-func (r *queryResolver) Genre(ctx context.Context, genre string, year int, month int) ([]*model.ArticleInfo, error) {
+func (r *queryResolver) Like(ctx context.Context, userid *string, articleid *string) (*model.Like, error) {
+	panic("not implemented")
+}
+
+func (r *queryResolver) List(ctx context.Context, userid *string, articleid *string) (*model.List, error) {
+	panic("not implemented")
+}
+
+func (r *queryResolver) Genres(ctx context.Context) (*model.Genres, error) {
+	panic("not implemented")
+}
+
+func (r *queryResolver) Articles(ctx context.Context, genre string, year int, month int) (*model.Articles, error) {
 	panic("not implemented")
 }
 
@@ -69,27 +87,15 @@ func (r *queryResolver) Article(ctx context.Context, id string) (*model.Article,
 	panic("not implemented")
 }
 
-func (r *queryResolver) Like(ctx context.Context, userid *string, articleid *string) (bool, error) {
+func (r *queryResolver) Log(ctx context.Context, id string, logID string) (*model.Log, error) {
 	panic("not implemented")
 }
 
-func (r *queryResolver) List(ctx context.Context, userid *string, articleid *string) ([]*model.List, error) {
+func (r *queryResolver) Logs(ctx context.Context, id *string) (*model.Logs, error) {
 	panic("not implemented")
 }
 
-func (r *queryResolver) Loglist(ctx context.Context, id *string) ([]*model.Log, error) {
-	panic("not implemented")
-}
-
-func (r *queryResolver) Log(ctx context.Context, id string, logID string) (*model.LogInfo, error) {
-	panic("not implemented")
-}
-
-func (r *queryResolver) Logs(ctx context.Context, id *string) ([]*model.LogInfo, error) {
-	panic("not implemented")
-}
-
-func (r *queryResolver) Spots(ctx context.Context, latitude float64, longitude float64, worktime int, emotion float64) ([]*model.Spot, error) {
+func (r *queryResolver) Spots(ctx context.Context, latitude float64, longitude float64, worktime int, emotion int) (*model.Spots, error) {
 	panic("not implemented")
 }
 
