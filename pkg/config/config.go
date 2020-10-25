42063b62a1d339b74908ad56bf1aaf3a575be2ef
SaKu2110 <b1018106@fun.ac.jp>
2020-10-26 05:05:42 +0900

    [Create] get list function

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,7 +2,20 @@ package config
 
 import(
 	"os"
+	"log"
+	"github.com/kelseyhightower/envconfig"
 )
+
+type APIPath struct {
+	Auth		string	`envconfig:"AUTH_API"`
+	User		string	`envconfig:"USER_API"`
+	Record		string	`envconfig:"RECORD_API" default:"https://backend-record.herokuapp.com"`
+	Memory		string	`envconfig:"MEMORY_API"`
+	Collection	string	`envconfig:"COLLECTION_API"`
+	Log			string	`envconfig:"LOG_API"`
+	Spot		string	`envconfig:"SPOT_API"`
+	Evaluation	string	`envconfig:"EVALUATION_API"`
+}
 
 func GetRouterAddr() (addr string) {
 	port := os.Getenv("PORT")
@@ -11,4 +24,13 @@ func GetRouterAddr() (addr string) {
 		addr = ":9020"
 	}
 	return
+}
+
+func GetAPIPath() APIPath {
+	var c APIPath
+	if err := envconfig.Process("", &c); err != nil {
+		log.Fatal(err)
+	}
+
+	return c
 }
