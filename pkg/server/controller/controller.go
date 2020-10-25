ca051f5ff1eefb59a4c8da8f6b65535d42dae020
SaKu2110 <b1018106@fun.ac.jp>
2020-10-25 17:59:14 +0900

    [Add] initialize controller function

--- pkg/server/controller/controller.go
+++ pkg/server/controller/controller.go
@@ -1,14 +1,39 @@
 package controller
 
 import(
+	"log"
+	"errors"
 	"context"
 	"net/http"
+	"runtime/debug"
 	"github.com/gin-gonic/gin"
+
+	// generate package
+	"github.com/miraikeitai2020/backend-bff/pkg/bff"
+	"github.com/miraikeitai2020/backend-bff/pkg/server/controller/resolver"
+
+	// gql-gen package
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 type Controller struct {
 	PlaygroundServer http.HandlerFunc
 	QueryServer http.Handler
+}
+
+func Init() Controller {
+	srv := handler.NewDefaultServer(bff.NewExecutableSchema(bff.Config{Resolvers: &resolver.Resolver{}}))
+	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
+		log.Print(err)
+		debug.PrintStack()
+		return errors.New("user message on panic")
+	})
+
+	return Controller{
+		PlaygroundServer: playground.Handler("GraphQL playground", "/query"),
+		QueryServer: queryMiddleware(srv),
+	}
 }
 
 func (ctrl *Controller) PlaygroundHandler(cx *gin.Context) {
@@ -19,7 +44,7 @@ func (ctrl *Controller) QueryHandler(cx *gin.Context) {
 	ctrl.QueryServer.ServeHTTP(cx.Writer, cx.Request)
 }
 
-func QueryMiddleware(next http.Handler) http.Handler {
+func queryMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.WithValue(r.Context(), "id", r.Header.Get("id"))
 		ctx = context.WithValue(ctx, "pass", r.Header.Get("pass"))
