93747a3d2d4c288180a87e8d2de746efe1edf39f
SaKu2110 <b1018106@fun.ac.jp>
2020-10-27 05:02:02 +0900

    [Create] data transer object package

---
+++ pkg/server/model/dto/query.go
@@ -0,0 +1,37 @@
+package dto
+
+import(
+	"github.com/miraikeitai2020/backend-bff/pkg/server/model"
+)
+
+type ArticlesResponse struct {
+	ArticleList []*model.ArticleHeader `json:"articleList"`
+}
+
+type ArticleResponse struct {
+	ArticleInfo model.ArticleInfo
+}
+
+type SpotResponse struct {
+	Spot SpotInfo `json:"spot"`
+}
+
+type SpotInfo struct {
+	ID			string	`json:"id"`
+	Name		string	`json:"name"`
+	Latitude	float64	`json:"latitude"`
+	Longitude	float64	`json:"longitude"`
+}
+
+type DetourResponse struct {
+	Detour []DetourInfo `json:"detours"`
+}
+
+type DetourInfo struct {
+	ID			string	`json:"id"`
+	Name		string	`json:"name"`
+	Image		string	`json:"image"`
+	Description	string	`json:"description"`
+	Latitude	float64	`json:"latitude"`
+	Longitude	float64	`json:"longitude"`
+}
